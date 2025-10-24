fd8f90a463730ffb7448645d37b83a5b5f94ce86
Nic Robinson <ntrpilot@gmail.com>
2025-10-24 19:52:58 +0100

    Cleanup fence utilities

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,22 +3,16 @@ using utils;
 
 public class Projectile : MonoBehaviour
 {
-    private ViewportPlane _viewportPlane;
     private Vector3 _velocity;
 
     public void Initialise(Camera targetCamera, Vector3 velocity)
     {
         _velocity = velocity;
-
-        _viewportPlane = ViewportPlane.CreateBoundingViewportPlane(targetCamera, gameObject, ViewportLock.Center, 100,
-            -10, ViewportLock.Center, 100, -10);
     }
 
     private void Update()
     {
         gameObject.transform.position += _velocity * Time.deltaTime;
-
-        if (_viewportPlane.IsOutOfViewportArea(gameObject.transform.position))
-            Destroy(gameObject);
+        if (CleanUpFence.ShouldDestroy(gameObject)) Destroy(gameObject);
     }
 }
---
+++ Assets/Scripts/utils/CleanUpFence.cs
@@ -0,0 +1,22 @@
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace utils
+{
+    /**
+     * A helper class that uses the CleanUpFence Game Object loaded into the scene to
+     * expose a facade API for working with the cleanup fence. Thing outside the fence
+     * area considered ready for cleanrup.
+     */
+    public static class CleanUpFence
+    {
+        [CanBeNull] private static readonly ViewportArea Instance =
+            GameObject.Find("CleanUpFence")?.GetComponent<ViewportArea>();
+        
+        public static bool ShouldDestroy(GameObject gameObject)
+        {
+            // If the game object was not found - just return false as we don't know
+            return Instance?.IsOutOfViewportArea(gameObject.transform.position) ?? false;
+        }
+    }
+}
